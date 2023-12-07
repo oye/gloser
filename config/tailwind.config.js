26524d237fa14252d6a9003270b747b5c2fc6952
Øyvind Guttvik Årnes <oyvind.arnes@hey.com>
2023-12-07 22:49:54 +0100

    Changed the style a bit, not sure

--- config/tailwind.config.js
+++ config/tailwind.config.js
@@ -12,6 +12,10 @@ module.exports = {
       fontFamily: {
         sans: ['Inter var', ...defaultTheme.fontFamily.sans],
       },
+      animation: {
+        // Bounces 5 times 1s equals 5 seconds
+        'bounce-short': 'bounce 1s ease-in-out 2'
+      }
     },
   },
   plugins: [
